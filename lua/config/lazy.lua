d96d490e62b67f56cc8054dacfde29b83abba0a5
jaimesoad <jaime.ac1106@gmail.com>
2024-02-07 13:05:37 -0500

    changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,12 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+vim.filetype.add({
+    extension = {
+        templ = "templ",
+    },
+})
+
 require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
